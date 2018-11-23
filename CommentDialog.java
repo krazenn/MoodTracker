9b679849d060eabfc82c4f5903f9b62ce60a9c7a
krazenn <antho13_1@msn.com>
2018-11-23 09:44:34 +0100

    Alertdialog mit dans méthode dialogComment Ajout SharedPreferences Ajout key SharedPreferences BUNDLE_CURRENT_THEME Permet de sauvegarder le smiley avec le background associer lors du click sur le bouton enregistrer.

---
+++ CommentDialog.java
@@ -0,0 +1,31 @@
+package com.example.kraken.moodtracker;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+
+public class CommentDialog extends DialogFragment {
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.saisir_commentaire_sur_votre_humeur)
+                .setPositiveButton(R.string.Enregistrer, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton(R.string.Annuler, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+
+
+}
--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.kraken.moodtracker;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -20,16 +21,24 @@ public class MainActivity extends AppCompatActivity {
     private int[] smileyImage = {R.drawable.smileyhappy, R.drawable.smileysuperhappy,R.drawable.smileysad, R.drawable.smileydisapointed, R.drawable.smileynormal};
     private int[] colorBackground = {R.color.light_sage, R.color.banana_yellow, R.color.faded_red, R.color.warm_grey, R.color.cornflower_blue_65};
 
+    public static final String BUNDLE_CURRENT_THEME = "BUNDLE_CURRENT_THEME";
+    private SharedPreferences sharedPref ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mLayout = findViewById(R.id.layout);
-        mImage = findViewById(R.id.imagehappy);
+        mImage = findViewById(R.id.ivSmiley);
         ImageButton imgBtnComment = findViewById(R.id.comment);
         ImageButton imgBtnNext = findViewById(R.id.next);
         ImageButton imgBtnHistory = findViewById(R.id.history);
+
+        sharedPref = getPreferences(MODE_PRIVATE);
+
+        mImage.setImageResource(smileyImage[sharedPref.getInt(BUNDLE_CURRENT_THEME,0) -1]);
+        mLayout.setBackgroundResource(colorBackground[sharedPref.getInt(BUNDLE_CURRENT_THEME,0) -1]);
 
         /*
         Permet de switch entre les différents smiley et la couleur du background
@@ -37,10 +46,10 @@ public class MainActivity extends AppCompatActivity {
         imgBtnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                currentImage++;
                 currentImage = currentImage % smileyImage.length;
                 mImage.setImageResource(smileyImage[currentImage]);
                 mLayout.setBackgroundResource(colorBackground[currentImage]);
+                currentImage++;
             }
         });
 
@@ -51,25 +60,30 @@ public class MainActivity extends AppCompatActivity {
         imgBtnComment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                LayoutInflater inflater = MainActivity.this.getLayoutInflater();
-                builder.setTitle("Humeur du jour");
-                builder.setView(inflater.inflate(R.layout.dialog_comment, null))
-                        .setPositiveButton(R.string.Enregistrer, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-                            }
-                        })
-                        .setNegativeButton(R.string.Annuler, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-
-                            }
-                        });
-                builder.create();
-                builder.show();
+                dialogComment();
                 }
         });
+        }
+
+        public void dialogComment (){
+    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+    LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+    builder.setTitle("Humeur du jour");
+    builder.setView(inflater.inflate(R.layout.dialog_comment, null))
+            .setPositiveButton(R.string.Enregistrer, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    sharedPref.edit().putInt(BUNDLE_CURRENT_THEME, currentImage).apply();
+
+                }
+            })
+            .setNegativeButton(R.string.Annuler, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+    builder.create();
+    builder.show();
     }
 }
