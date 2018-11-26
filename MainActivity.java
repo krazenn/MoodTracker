9701b8479c497010515283b5eb0125d1d4f993cc
krazenn <antho13_1@msn.com>
2018-11-26 19:14:39 +0100

    SharedPreferences sauvegarde et reaffiche le Theme en cours, et le dernier commentaire

--- MainActivity.java
+++ MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.kraken.moodtracker;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -21,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
     private int[] colorBackground = {R.color.light_sage, R.color.banana_yellow, R.color.faded_red, R.color.warm_grey, R.color.cornflower_blue_65};
 
     public static final String BUNDLE_CURRENT_THEME = "BUNDLE_CURRENT_THEME";
+    public static final String BUNDLE_COMMENT = "BUNDLE_COMMENT";
     private SharedPreferences sharedPref ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
         ImageButton imgBtnHistory = findViewById(R.id.iBtnHistory);
 
         sharedPref = getPreferences(MODE_PRIVATE);
-
         mImage.setImageResource(smileyImage[sharedPref.getInt(BUNDLE_CURRENT_THEME,0) -1]);
         mLayout.setBackgroundResource(colorBackground[sharedPref.getInt(BUNDLE_CURRENT_THEME,0) -1]);
         currentImage = sharedPref.getInt(BUNDLE_CURRENT_THEME,0);
@@ -65,15 +69,25 @@ public class MainActivity extends AppCompatActivity {
         });
         }
 
+        @SuppressLint("SetTextI18n")
         public void dialogComment (){
-    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-    LayoutInflater inflater = MainActivity.this.getLayoutInflater();
-    builder.setTitle("Humeur du jour");
-    builder.setView(inflater.inflate(R.layout.dialog_comment, null))
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+            builder.setTitle("Humeur du jour");
+            View v = inflater.inflate(R.layout.dialog_comment, null);
+            final EditText commentInput = v.findViewById(R.id.commentInput);
+            commentInput.setText(sharedPref.getString(BUNDLE_COMMENT, null));
+
+            builder.setView(v)
             .setPositiveButton(R.string.Enregistrer, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
+                    String comment;
+                    comment = commentInput.getText().toString();
                     sharedPref.edit().putInt(BUNDLE_CURRENT_THEME, currentImage).apply();
+                    sharedPref.edit().putString(BUNDLE_COMMENT, comment).apply();
+
+
 
                 }
             })
